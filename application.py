78709eb6e44af897fad3cefaaf0d8436b394c5f7
joselxes <jose.revelo@yachaytech.edu.ec>
2019-11-18 22:11:59 -0500

    pre creacion del login and register(aparte)

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -15,12 +15,32 @@ if not os.getenv("DATABASE_URL"):
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-# Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
+# Set up database
 db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return  render_template("logPage.html")
+
+@app.route("/welcome", methods=["GET","POST"])
+def welcome():
+    if request.method=="POST":
+        session.name = request.form.get("name")
+        session.contrasena = request.form.get("psw")
+        name = request.form.get("name")
+        contrasena = request.form.get("psw")
+        return  render_template("welcome.html",
+        name=session.name,
+        contrasena=session.contrasena)
+    else:
+        return render_template(" advertencia.html")
+
+@app.route("/welcomeNewUser")
+def welcomeNewUser():
+    return  render_template("welcomeNewUser.html")
+
+# @app.route("/welcomeNewUser/portal")
+# def portal():
+#     return  render_template("portal.html")
