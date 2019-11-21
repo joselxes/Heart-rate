0d3a92d2b749c25edd04f2190b84498ffdba37f3
joselxes <jose.revelo@yachaytech.edu.ec>
2019-11-21 05:35:42 -0500

    log casi todo, db done, import done

--- application.py
+++ application.py
@@ -20,20 +20,31 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def index():
+    if session.get("name") is None:
+        session["name"] = []
+        session["contrasena"] = ""
+        session["session"] = "si"
+    if request.method == "POST":
+        session["session"]=request.form.get("session")
+        if session["session"] == "no" :
+            session.clear()
+            session["name"] = []
+            session["contrasena"] = ""
+            session["session"] = "si"
     return  render_template("trueLogin.html")
-    # return  render_template("logPage.html")
+
 
 @app.route("/welcome", methods=["GET","POST"])
 def welcome():
     if request.method=="POST":
-        session.name = request.form.get("name")
-        session.contrasena = request.form.get("psw")
+        session["name"] = request.form.get("name")
+        session["contrasena"] = request.form.get("psw")
         # session["notes"].append(note)
         return  render_template("welcome.html",
-        name=session.name,
-        contrasena=session.contrasena)
+        name=session["name"],
+        contrasena=session["contrasena"])
     else:
         return render_template(" advertencia.html")
 
